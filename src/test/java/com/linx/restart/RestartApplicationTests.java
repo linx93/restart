99168ffdddf291c559fb07f62d0a1ec934c84b6d
linx <1824517828@qq.com>
2022-06-20 23:27:30 +0800

    init repo

---
+++ src/test/java/com/linx/restart/RestartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.linx.restart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
